84a58f0e1b77b30dea63a1e9dc162b1f535c5699
Jai Smith <jksmithnyc@gmail.com>
2020-04-21 21:10:24 -0400

    minor aesthetic changes, added fenceable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+  Switch,
+  Redirect,
 } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -12,34 +13,39 @@ import Project from './components/Project';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Header />
         <Switch>
-          <Route path='/'>
-            <Project 
-              name='ITC Wireless Tool'
-              org='DALI Lab'
-              date='Winter 2020'
-              blurb='Dartmouth College is currently undergoing a multi-million dollar campus-wide upgrade to WiFi infrastructure. In order to prioritize upcoming building upgrades in high-traffic areas, Dartmouth ITC hired the DALI Lab to build a WiFi reporting tool that taps into Dartmouth’s networking data to track issues and overloaded access points. This project is in the final stages of development and is on track for a public launch in Spring 2020.'
-              img={{
-                src: require('./media/wirelesstool.png'),
-                alt: 'Wireless Tool web app on Macbook Pro'
-              }}
-            />
-            <Project 
-              name='ITC Wireless Tool'
-              org='DALI Lab'
-              date='Winter 2020'
-              blurb='Dartmouth College is currently undergoing a multi-million dollar campus-wide upgrade to WiFi infrastructure. In order to prioritize upcoming building upgrades in high-traffic areas, Dartmouth ITC hired the DALI Lab to build a WiFi reporting tool that taps into Dartmouth’s networking data to track issues and overloaded access points. This project is in the final stages of development and is on track for a public launch in Spring 2020.'
-              img={{
-                src: require('./media/wirelesstool.png'),
-                alt: 'Wireless Tool web app on Macbook Pro'
-              }}
-              flipped
-            />
+          <Route path='/projects'>
+            <div className="app-projects">
+              <Project 
+                name='ITC Wireless Tool'
+                org='DALI Lab'
+                date='Winter 2020'
+                blurb='Dartmouth College is currently undergoing a multi-million dollar campus-wide upgrade to WiFi infrastructure. In order to prioritize upcoming building upgrades in high-traffic areas, Dartmouth ITC hired the DALI Lab to build a WiFi reporting tool that taps into Dartmouth’s networking data to track issues and overloaded access points. This project is in the final stages of development and is on track for a public launch in Spring 2020.'
+                img={{
+                  src: require('./media/wirelesstool.png'),
+                  alt: 'Wireless Tool web app on Macbook Pro'
+                }}
+              />
+              <Project 
+                name='Fenceable'
+                org='ENGS 021'
+                date='Fall 2019'
+                blurb='A wearable rack to facilitate easy deployment and collection of temporary electric fencing. Management-Intensive Rotational Grazing (which uses the aforementioned fencing) is a rapidly growing practice among organic farmers in the U.S., but no products currently exist on the market to facilitate its use. This product is U.S. Patent Pending as of November 2019, expiring November 2020.'
+                img={{
+                  src: require('./media/fenceable.png'),
+                  alt: 'Fenceable product image on white background'
+                }}
+                flipped
+              />
+            </div>
           </Route>
           <Route path='/resume'>
-            
+            <p>this is my resume</p>
+          </Route>
+          <Route path='/'>
+            <Redirect to='/projects' />
           </Route>
         </Switch>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
-  BrowserRouter as Router,
-  Link,
+  NavLink,
   withRouter
 } from 'react-router-dom'
 import './Header.scss';
@@ -18,61 +17,65 @@ class Header extends PureComponent {
     };
   }
 
-  componentWillMount = () => {
-    console.log('Header will mount');
-  }
+  // componentWillMount = () => {
+  //   console.log('Header will mount');
+  // }
 
-  componentDidMount = () => {
-    console.log('Header mounted');
-  }
+  // componentDidMount = () => {
+  //   console.log('Header mounted');
+  // }
 
-  componentWillReceiveProps = (nextProps) => {
-    console.log('Header will receive props', nextProps);
-  }
+  // componentWillReceiveProps = (nextProps) => {
+  //   console.log('Header will receive props', nextProps);
+  // }
 
-  componentWillUpdate = (nextProps, nextState) => {
-    console.log('Header will update', nextProps, nextState);
-  }
+  // componentWillUpdate = (nextProps, nextState) => {
+  //   console.log('Header will update', nextProps, nextState);
+  // }
 
-  componentDidUpdate = () => {
-    console.log('Header did update');
-  }
+  // componentDidUpdate = () => {
+  //   console.log('Header did update');
+  // }
 
-  componentWillUnmount = () => {
-    console.log('Header will unmount');
-  }
+  // componentWillUnmount = () => {
+  //   console.log('Header will unmount');
+  // }
 
   render () {
     return (
-      <Router>
-        <div className="header">
-          <div className="header-box">
-            <img
-              className="header-pic"
-              src={profile}
-              alt="Self-Portrait"
-            />
-            <div className="header-title">
-              <div className="header-title-main">
-                Jai K. Smith
-              </div>
-              <div className="header-title-sub">
-                Software Engineer, Dartmouth Undergrad<br />
-                <i>New York, NY</i>
-              </div>
+      <div className="header">
+        <div className="header-box">
+          <img
+            className="header-pic"
+            src={profile}
+            alt="Self-Portrait"
+          />
+          <div className="header-title">
+            <div className="header-title-main">
+              Jai K. Smith
+            </div>
+            <div className="header-title-sub">
+              Software Engineer, Dartmouth Undergrad<br />
+              <i>New York, NY</i>
             </div>
           </div>
-          <div className="header-nav">
-            <div className="header-active-highlight" />
-            <Link to='/projects' id='active'>
-              Projects
-            </Link>
-            <Link to='/resume'>
-              Resume
-            </Link>
-          </div>
         </div>
-      </Router>
+        <div className="header-nav">
+          <div className="header-active-highlight" />
+          <NavLink 
+            className="header-nav-item"
+            activeClassName="selected"
+            to='/projects'>
+            Projects
+          </NavLink>
+          <NavLink
+            className="header-nav-item"
+            activeClassName="selected"
+            to='/resume'>
+            Resume
+          </NavLink>
+        </div>
+      </div>
     );
   }
 }
