45689d4fbd3a3ef20777f89281b8925a8bc4f9bc
Jai Smith <jksmithnyc@gmail.com>
2020-05-05 13:38:06 -0400

    pages

---
+++ precache-manifest.4f49f06aae54e1e159d43aec8924e632.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82dc4c140105cb594df16a25ed2798c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65eff2a3c4e47d34eb4b",
+    "url": "/static/css/main.5f73c795.chunk.css"
+  },
+  {
+    "revision": "52d3e9b20b892502dcb3",
+    "url": "/static/js/2.40350367.chunk.js"
+  },
+  {
+    "revision": "96fceeb0dd6f3045b65a922aef51943a",
+    "url": "/static/js/2.40350367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65eff2a3c4e47d34eb4b",
+    "url": "/static/js/main.2593f34b.chunk.js"
+  },
+  {
+    "revision": "0f54c7a00dfc6b91dd23",
+    "url": "/static/js/runtime-main.37dcd6d1.js"
+  },
+  {
+    "revision": "399d1e3a54973a975c07ded7cc467353",
+    "url": "/static/media/fenceable.399d1e3a.png"
+  },
+  {
+    "revision": "a67208c5a7e31a8464fb29805c5f2929",
+    "url": "/static/media/kv.a67208c5.png"
+  },
+  {
+    "revision": "82b5733fb580ed0314cdda72efabae7f",
+    "url": "/static/media/profile.82b5733f.jpeg"
+  },
+  {
+    "revision": "b517b51cc96a89546c3bdab8f58b878f",
+    "url": "/static/media/wirelesstool.b517b51c.png"
+  }
+]);
