ea6823cf3c3cd56584f1e094db28367866d35ca5
Jai <jksmithnyc@gmail.com>
2020-08-04 03:06:28 -0400

    fixed google analytics

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ReactGA from 'react-ga';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 import reducers from './reducers';
-
-// initialize google analytics
-ReactGA.initialize('UA-145221220-1');
 
 // create redux store
 const store = createStore(reducers, {}, compose(
@@ -19,7 +16,9 @@ const store = createStore(reducers, {}, compose(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
